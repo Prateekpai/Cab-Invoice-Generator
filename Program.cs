05bcf54e3d8bca0b71597085ddda57fb45381f84
Prateekpai <prateekvasanthpai@gmail.com>
2022-04-19 19:05:16 +0530

    Welcome to Cab Invoice Generator

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Cabinvoicegenerator
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to Cab Invoice Generator Problem");
+        }
+    }
+}
